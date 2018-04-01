0eaa9d8c82ab070640eacd67753df9e377ba6194
Akash-Pardasani <smartavi004@gmail.com>
2018-04-01 21:42:48 +0530

    Update store_to_nparray.py

--- store_to_nparray.py
+++ store_to_nparray.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-import cv2 as cv
+from scipy.misc import imread, imsave, imresize
 
 test_data = []
 train_data = []
@@ -9,17 +9,22 @@ face_data = []
 
 
 for i in range(1,7):
-	f_path = ("/home/akash/Desktop/{0}/Faces/".format(i))
-	c_path = ("/home/akash/Desktop/{0}/Crowd/".format(i))	
-	while images in f_path:
-		im = cv.imread(images)
+	f_path = ("/home/akash/Desktop/{0}/Faces".format(i))
+	c_path = ("/home/akash/Desktop/{0}/Crowd".format(i))
+		
+	for images in os.listdir(f_path):
+		image = os.path.join(f_path, images)
+		im = imread(image)
 		face_data.append((im, i))
-		print i
-	while images1 in c_path:
-		im1 = cv.imread(images1)	
+		print "face", i
+	for images1 in os.listdir(c_path):
+		image1 = os.path.join(c_path, images1)
+		im1 = imread(image1)	
 		crowd_data.append((im1, 7))
+		print "crowd", i
 
-crowd_data = crowd_data[:3500]
+np.random.shuffle(crowd_data)
+crowd_data = crowd_data[:4000]
 for tup in crowd_data:
 	train_data.append(tup)
 for tup in face_data:
@@ -27,9 +32,12 @@ for tup in face_data:
 
 np.random.shuffle(train_data)
 np.random.shuffle(train_data)
-length = train_data.shape[1]
+length = len(train_data)
 test_length = 0.1*length
-test_data = train_data[:test_length]
-train_data = [test_length:]
+test_data = train_data[:int(test_length)]
+train_data = train_data[int(test_length):]
 np.save("test.npy", test_data)
-np.save("train.npy", train_data)	
+np.save("train.npy", train_data)
+test1 = np.load("train.npy")
+for a in test1:
+	print a[0].shape	
