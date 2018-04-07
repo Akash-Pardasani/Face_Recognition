b610e485702e8a9add28159adf7d913c672913fb
Archit Aggarwal <archit522@gmail.com>
2018-04-07 20:27:56 +0530

    Minor Changes

--- gradient_ascent.py
+++ gradient_ascent.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
+Created on Sat Apr  7 17:38:14 2018
+
+@author: architaggarwal
+"""
+
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Apr  5 00:36:38 2018
 
 @author: architaggarwal
@@ -13,23 +21,24 @@ import argparse
 import keras
 import matplotlib.pyplot as plt
 import numpy as np
+from keras.models import load_model
 from keras.applications.vgg16 import preprocess_input, VGG16
 from keras import backend as K
-imageName = str("drive/app/atul.jpg")
+imageName = str("/Users/architaggarwal/Downloads/atul.jpg")
 image = image_utils.load_img(imageName, target_size=(224, 224))
 image = image_utils.img_to_array(image)
 image = np.expand_dims(image, axis=0)
 image = preprocess_input(image)
 input_img = image
 image_preprocessed=np.rollaxis(image,1,4)
-model = keras.applications.vgg16.VGG16()
+model = load_model("/Users/architaggarwal/Downloads/model-2.h5")
 
 def get_featuremaps(model, layer_idx, X_batch):
 	get_activations = K.function([model.layers[0].input],[model.layers[layer_idx].output])
 	activations = get_activations([X_batch,0])
 	return(activations)
-
-activations = get_featuremaps(model, len(model.layers)-18, image)
+x = len(model.layers)-6
+activations = get_featuremaps(model, x, image)
 #plt.imshow(self.activations[0][0][63],cmap = 'gray')
 print(type(activations))
 print(len(activations))
@@ -38,17 +47,17 @@ output_shape = np.shape(activations[0])
 featuremap_size = np.shape(activations[0][0][0])
 num_of_featuremaps = (np.shape(activations[0][0]))[2]
 print(num_of_featuremaps)
-layer_info=model.layers[len(model.layers)-18].get_config()
+layer_info=model.layers[x].get_config()
 layer_name=layer_info['name']
-input_shape=model.layers[len(model.layers)-18].input_shape
-layer_param=model.layers[len(model.layers)-18].count_params()
+input_shape=model.layers[x].input_shape
+layer_param=model.layers[x].count_params()
 if len(output_shape)==2:
-	fig=plt.figure(figsize=(100,100))
+	fig=plt.figure(figsize=(50,50))
 	plt.imshow(activations[0].T,cmap='gray')
-	plt.savefig("drive/app/featuremaps-layer-{}".format(len(model.layers)-18) + '.png')
+	plt.savefig("/Users/architaggarwal/Downloads/featuremaps-layer-{}-atul".format(x) + '.png')
 
 else:
-	fig=plt.figure(figsize=(100,100))
+	fig=plt.figure(figsize=(50,50))
 	subplot_num=int(np.ceil(np.sqrt(num_of_featuremaps)))
 	print(subplot_num)
 	for i in range(int(num_of_featuremaps)):
@@ -60,4 +69,5 @@ else:
 		plt.yticks(np.array([]))
 		#plt.tight_layout()
 	plt
-	plt.savefig("drive/app/featuremaps-layer-{}".format(len(model.layers)-18) + '.png')
+	plt.savefig("/Users/architaggarwal/Downloads/featuremaps-layer-{}-atul".format(x) + '.png')
+
